d984752892edb985a9e5915b2e2f3332029d162b
LachezarP <lachezarpeychev.ca@gmail.com>
2019-09-19 15:19:26 -0400

    +Welcome screen finalized +Activity screen   -Added beter color and background

--- app/src/main/java/com/example/lachoshop/WelcomeScreen.java
+++ app/src/main/java/com/example/lachoshop/WelcomeScreen.java
@@ -1,7 +1,9 @@
 package com.example.lachoshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(WelcomeScreen.this, MainActivity.class);
+        startActivity(intent);
     }
 }
